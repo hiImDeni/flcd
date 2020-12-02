c7828b6dd0d9ab27582d55be9e2e1a06c4af97e8
sekilm <b_lascau@yahoo.com>
2020-12-02 15:55:10 +0200

    Progress yay

--- lab5-parser/Parser.py
+++ lab5-parser/Parser.py
@@ -7,7 +7,7 @@ class Parser:
     def __init__(self, grammar):
         self.grammar = grammar
 
-    def closure(self, element):
+    def closure(self, element, productions):
         # element is a tuple of the form (start, productions[start])
 
         c = {element[0]: element[1]}
@@ -18,7 +18,7 @@ class Parser:
                 keys = list(c.keys())
                 for j in keys[i]:
                     if j in self.grammar.non_terminals:
-                        for k in self.grammar.productions[j]:
+                        for k in productions[j]:
                             if len(k) == 1 and k[0] == '0':
                                 pass
                             if j not in c.keys():
@@ -32,7 +32,7 @@ class Parser:
                 for symbol in el:
                     if el.index(symbol) > index:
                         if symbol in self.grammar.non_terminals:
-                            for k in self.grammar.productions[symbol]:
+                            for k in productions[symbol]:
                                 if len(k) == 1 and k[0] == '0':
                                     pass
                                 if symbol not in c.keys():
@@ -42,56 +42,55 @@ class Parser:
             if aux == c:
                 return c
 
-    def goto(self, state, symbol):
+    def goto(self, state, symbol, productions):
         for el in state:
-            list = state[el]
-            for values in list:
+            rhp = copy.deepcopy(state[el])
+
+            for values in rhp:
                 for i in range(len(values)):
                     if i != len(values) - 1:
-                        if values[i] == '.' and values[i+1] == symbol:
-                            aux = values[i]
-                            values[i] = values[i+1]
-                            values[i+1] = aux
+                        if values[i] == '.' and values[i + 1] == symbol:
 
-                            return self.closure((el, state[el]))
+                            aux = values[i]
+                            values[i] = values[i + 1]
+                            values[i + 1] = aux
+
+                            return self.closure((el, rhp), productions)
         return []
 
     def ColCan(self):
         start = self.grammar.start
-        c = []
+        productionsCopy = copy.deepcopy(self.grammar.productions)
+
         states = []
 
-        # self.grammar.productions[start][0].insert(0, '.')
-        for nt in self.grammar.productions:
-            for production in self.grammar.productions[nt]:
+        for nt in productionsCopy:
+            for production in productionsCopy[nt]:
                 production.insert(0, '.')
 
-        s0 = self.closure((start, self.grammar.productions[start]))
-        print(s0)
-        print(self.grammar.productions[start])
+        s0 = self.closure((start, productionsCopy[start]), productionsCopy)
 
-        c.append(s0)
         states.append(s0)
+        print(states)
 
         while True:
-            aux = c
+            aux = states
             nr = 0
-            for state in c:
+            for state in states:
+                stateCopy = copy.deepcopy(state)
                 for symbol in self.grammar.non_terminals + self.grammar.terminals:
                     if symbol != self.grammar.start:
-                        gotoResult = self.goto(state, symbol)
-                        if gotoResult != []:
+                        gotoResult = self.goto(state, symbol, productionsCopy)
+                        if gotoResult:
                             print(symbol + " " + str(nr))
                             print(gotoResult)
                             print()
-                        if gotoResult is not [] and gotoResult not in c:
-                            c.append(gotoResult)
+                        if gotoResult is not [] and gotoResult not in states:
                             states.append(gotoResult)
                 nr += 1
 
-            if aux == c:
-                break
-        return states
+            if aux == states:
+                return states
 
     def construct_table(self):
         c = self.ColCan()
@@ -101,34 +100,36 @@ class Parser:
 
     def parse(self, word):
         states = self.ColCan()
+
+        print(states)
+        print(self.grammar.productions)
+
         beta = list(word)
         index = 0
         alpha = [index]
         phi = []
         end = False
 
-        while not end:
-            state = states[index]
-            if self.action(state) == 'shift':
-                a = beta.pop(0)
-                state = self.goto(state, a)
-                alpha.append(a)
-                alpha.append(index)
-            elif self.action(state) == 'accept':
-                print('success')
-                end = True
-            elif self.action(state) == 'error':
-                print('error')
-                end = True
-            elif len(self.action(state)) == 2 and self.action(state)[0] == 'reduce':
-                l = self.action(state)[1]
-                self.search_production(l)
-                state = self.goto()
-                alpha.pop()
-                alpha.pop()
-                phi.append(l)
-
-        print(states)
+        # while not end:
+        #    state = states[index]
+        #    if self.action(state) == 'shift':
+        #        a = beta.pop(0)
+        #        state = self.goto(state, a)
+        #        alpha.append(a)
+        #        alpha.append(index)
+        #    elif self.action(state) == 'accept':
+        #       print('success')
+        #        end = True
+        #    elif self.action(state) == 'error':
+        #       print('error')
+        #        end = True
+        # elif len(self.action(state)) == 2 and self.action(state)[0] == 'reduce':
+        #    l = self.action(state)[1]
+        #    self.search_production(l)
+        #    state = self.goto()
+        #    alpha.pop()
+        #   alpha.pop()
+        #    phi.append(l)
 
     def search_production(self, l):
         pass
@@ -136,5 +137,5 @@ class Parser:
 
 grammar = Grammar()
 parser = Parser(grammar)
-word = input("Enter a word: ")
-parser.parse(word)
+# word = input("Enter a word: ")
+parser.parse("a")
